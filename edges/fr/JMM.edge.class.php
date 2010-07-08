8c206bb818901795afe6e01a778a0a6d3a00f657
brunoperel <brunoperel@f9fbd3f4-65e0-4270-b5f8-9c5e5b93a1e4>
2010-07-08 07:08:10 +0000

    Tranches Mickey Mystère et Mickey Aventure

---
+++ edges/fr/JMM.edge.class.php
@@ -0,0 +1,45 @@
+<?php
+class JMM extends Edge {
+    var $pays='fr';
+    var $magazine='JMM';
+    var $intervalles_validite=array(array('debut'=>1 , 'fin'=>9));
+    static $largeur_defaut=9.5;
+    static $hauteur_defaut=214;
+
+    function JMM ($numero) {
+        $this->numero=$numero;
+        $this->hauteur=214*Edge::$grossissement;
+        $this->largeur=8*Edge::$grossissement;
+        $this->image=imagecreatetruecolor(intval($this->largeur),intval($this->hauteur));
+    }
+
+    function dessiner() {
+        $image_texte=imagecreatetruecolor($this->hauteur,$this->largeur);
+        $noir=imagecolorallocate($image_texte, 0, 0, 0);
+        if ($this->numero==6)
+            $couleur_texte=imagecolorallocate ($image_texte, 255, 255, 255);
+        else
+            $couleur_texte=imagecolorallocate ($image_texte, 221, 133, 0);
+        
+        $titre=new Texte('MICKEY MYSTERE',0,8*Edge::$grossissement,
+							 8*Edge::$grossissement,0,$couleur_texte,'Boton Bold.ttf');
+        $titre->dessiner($image_texte);
+        $image_texte=imagerotate($image_texte, 90, $couleur_texte);
+        list($width,$height)=array(imagesx($image_texte),imagesy($image_texte));
+		$nouvelle_hauteur=($this->largeur)*($height/$width)*3/4;
+        imagecopyresampled ($this->image, $image_texte, 0, $this->hauteur/4, 0, 0, $this->largeur, $nouvelle_hauteur, $width, $height);
+
+
+        $image_numero=imagecreatetruecolor(2.5*$this->largeur,$this->largeur);
+        $numero=new Texte('N°'.$this->numero,0,8*Edge::$grossissement,
+							 8*Edge::$grossissement,0,$couleur_texte,'Boton Bold.ttf');
+        $numero->dessiner($image_numero);
+        $image_numero=imagerotate($image_numero, 90, $couleur_texte);
+        list($width,$height)=array(imagesx($image_numero),imagesy($image_numero));
+		$nouvelle_hauteur=($this->largeur)*($height/$width)*3/4;
+        imagecopyresampled ($this->image, $image_numero, 0, $this->largeur/2, 0, 0, $this->largeur, $nouvelle_hauteur, $width, $height);
+
+        return $this->image;
+    }
+}
+?>
