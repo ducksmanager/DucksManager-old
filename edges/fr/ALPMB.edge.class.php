0d81739019a6aa6e07dec7f8e448430dfdcbf781
brunoperel <brunoperel@f9fbd3f4-65e0-4270-b5f8-9c5e5b93a1e4>
2010-07-31 17:22:49 +0000

    Structure pour les ALPMB récents Ajout des numéros 49 et 57

--- edges/fr/ALPMB.edge.class.php
+++ edges/fr/ALPMB.edge.class.php
@@ -2,7 +2,7 @@
 class ALPMB extends Edge {
     var $pays='fr';
     var $magazine='ALPMB';
-    var $intervalles_validite=array(25,27);
+    var $intervalles_validite=array(25,27,49,57);
 
     var $en_cours=array();
     static $largeur_defaut=18;
@@ -10,11 +10,16 @@ class ALPMB extends Edge {
 
     function ALPMB($numero) {
         $this->numero=$numero;
-        if ($this->numero<=27) {
-            if ($this->numero >= 25) {
-                $this->largeur=18*Edge::$grossissement;
-                $this->hauteur=282*Edge::$grossissement;
-            }
+        if ($this->numero<=24) {
+
+        }
+        elseif ($this->numero<=27) {
+            $this->largeur=18*Edge::$grossissement;
+            $this->hauteur=282*Edge::$grossissement;
+        }
+        elseif ($this->numero <= 57) {
+            $this->largeur=18*Edge::$grossissement;
+            $this->hauteur=280*Edge::$grossissement;
         }
         $this->image=imagecreatetruecolor(intval($this->largeur),intval($this->hauteur));
         if ($this->image===false)
@@ -22,33 +27,41 @@ class ALPMB extends Edge {
     }
 
     function dessiner() {
-        if ($this->numero<=27) {
-            if ($this->numero >= 25) {
-                include_once($this->getChemin().'/../classes/MyFonts.Post.class.php');
+        if ($this->numero <= 24) {
 
-                $image2=imagecreatetruecolor($this->hauteur, $this->largeur);
-                $blanc=imagecolorallocate($image2, 255, 255, 255);
-                list($rouge,$vert,$bleu)=$this->getColorsFromDB(array(255,255,255));
-                $fond=imagecolorallocate($image2, $rouge, $vert, $bleu);
-                imagefill($image2, 0, 0, $fond);
-                $this->image=imagecreatetruecolor($this->hauteur, $this->hauteur);
-                list($rouge_texte,$vert_texte,$bleu_texte)=$this->getColorsFromDB(array(255,255,255),'Texte');
-                $post=new MyFonts('urw/nimbus-sans/l-black-condensed-italic',
-                                  rgb2hex($rouge_texte, $vert_texte, $bleu_texte),
-                                  rgb2hex($rouge, $vert, $bleu),
-                                  800,
-                                  'ALBUM PICSOU N°'.$this->numero,
-                                  42);
-                $chemin_image=$post->chemin_image;
-                list($texte,$width,$height)=imagecreatefromgif_getimagesize($chemin_image);
-                $nouvelle_largeur=$this->largeur*($width/$height)*0.8;
-                imagecopyresampled ($image2, $texte, $this->hauteur*0.2, $this->largeur*0.1, 0, 0, $nouvelle_largeur, $this->largeur*0.8, $width, $height);
-                
-                $this->image=imagerotate($image2, 90, $blanc);
-                if ($this->numero==25) {
-                    $this->placer_image ('/ALPMB.'.$this->numero.'.detail.png');
-                }
+        }
+        elseif ($this->numero<=27) {
+            include_once($this->getChemin().'/../classes/MyFonts.Post.class.php');
+
+            $image2=imagecreatetruecolor($this->hauteur, $this->largeur);
+            $blanc=imagecolorallocate($image2, 255, 255, 255);
+            list($rouge,$vert,$bleu)=$this->getColorsFromDB(array(255,255,255));
+            $fond=imagecolorallocate($image2, $rouge, $vert, $bleu);
+            imagefill($image2, 0, 0, $fond);
+            $this->image=imagecreatetruecolor($this->hauteur, $this->hauteur);
+            list($rouge_texte,$vert_texte,$bleu_texte)=$this->getColorsFromDB(array(255,255,255),'Texte');
+            $post=new MyFonts('urw/nimbus-sans/l-black-condensed-italic',
+                              rgb2hex($rouge_texte, $vert_texte, $bleu_texte),
+                              rgb2hex($rouge, $vert, $bleu),
+                              800,
+                              'ALBUM PICSOU N°'.$this->numero,
+                              42);
+            $chemin_image=$post->chemin_image;
+            list($texte,$width,$height)=imagecreatefromgif_getimagesize($chemin_image);
+            $nouvelle_largeur=$this->largeur*($width/$height)*0.8;
+            imagecopyresampled ($image2, $texte, $this->hauteur*0.2, $this->largeur*0.1, 0, 0, $nouvelle_largeur, $this->largeur*0.8, $width, $height);
+
+            $this->image=imagerotate($image2, 90, $blanc);
+            if ($this->numero==25) {
+                $this->placer_image ('/ALPMB.'.$this->numero.'.detail.png');
             }
+        }
+        elseif ($this->numero <= 57) {
+            list($rouge,$vert,$bleu)=$this->getColorsFromDB(array(255,255,255));
+            $fond=imagecolorallocate($this->image, $rouge, $vert, $bleu);
+            imagefill($this->image, 0, 0, $fond);
+            $this->placer_image('Logo ALPBM.png');
+            $this->placer_image('ALPMB.icone.'.$this->numero.'.png', 'bas');
         }
         return $this->image;
     }
