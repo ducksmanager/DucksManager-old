1a4b646b7b0020d55e8e11747ccd324a71e0b946
brunoperel <brunoperel@f9fbd3f4-65e0-4270-b5f8-9c5e5b93a1e4>
2010-09-07 17:50:56 +0000

    Ajout de tous les JM non-doubles restants

--- edges/fr/JM.edge.class.php
+++ edges/fr/JM.edge.class.php
@@ -3,13 +3,7 @@ class JM extends Edge {
     var $pays='fr';
     var $magazine='JM';
     static $numeros_doubles=array('2411-12','2463-64','2479-80','2506-07','2515-16','2531-32','2558-59','2584-85','2610-11','2619-20','2636-37','2662-63','2671-72','2688-89','2715-16','2723-24','2767-68','2819-20','2828-29','2844-45','2871-72','2879-80','2896-97','2923-24','2932-33','2948-49','2975-76','2984-85');
-    var $intervalles_validite=array(array('debut'=>500 , 'fin'=>1069),
-                                    array('debut'=>1229, 'fin'=>1322),
-                                    array('debut'=>1323, 'fin'=>1553),
-                                    array('debut'=>1558, 'fin'=>1942),
-                                    array('debut'=>1943, 'fin'=>2249),
-                                    array('debut'=>2400, 'fin'=>2499),
-                                    array('debut'=>2800, 'fin'=>3023,'sauf'=>array('2506-07','2515-16','2531-32','2558-59','2584-85','2610-11','2619-20','2636-37','2662-63','2671-72','2688-89','2715-16','2723-24','2767-68','2819-20','2828-29','2844-45','2871-72','2879-80','2896-97','2923-24','2932-33','2948-49','2975-76','2984-85')));
+    var $intervalles_validite=array(array('debut'=>1, 'fin'=>3023,'sauf'=>array('2506-07','2515-16','2531-32','2558-59','2584-85','2610-11','2619-20','2636-37','2662-63','2671-72','2688-89','2715-16','2723-24','2767-68','2819-20','2828-29','2844-45','2871-72','2879-80','2896-97','2923-24','2932-33','2948-49','2975-76','2984-85')));
     static $largeur_defaut=5;
     static $hauteur_defaut=275;
 
@@ -23,29 +17,34 @@ class JM extends Edge {
         elseif ($this->numero >=2963) {
             $this->hauteur=275;
         }
-        elseif ($this->numero >=2800) {
+        elseif ($this->numero >=2500) {
             $this->hauteur=285;
+            $this->largeur=6;
         }
-        elseif ($this->numero >= 2400 && $this->numero < 2500) {
+        elseif ($this->numero >=2400) {
             $this->hauteur=279;
+            $this->largeur=4;
         }
-        elseif ($this->numero >=1973 & $this->numero <= 2249) {
+        elseif ($this->numero >=1973) {
             $this->hauteur=270;
         }
-        elseif ($this->numero >=1558 & $this->numero <= 1942) {
+        elseif ($this->numero >=1554) {
             $this->hauteur=285;
+            $this->largeur=6;
         }
-        elseif ($this->numero >=1323 & $this->numero <= 1553) {
+        elseif ($this->numero >=1323) {
             $this->hauteur=295;
         }
-        elseif ($this->numero >=1229 & $this->numero <= 1322) {
+        elseif ($this->numero >=1229) {
             $this->hauteur=298;
         }
-        elseif ($this->numero >=500 & $this->numero <= 1069) {
-            $this->hauteur=312;
+        elseif ($this->numero >=500) {
+            $this->hauteur=295;
+            $this->largeur=6;
         }
         else {
-            $this->hauteur=279;
+            $this->hauteur=297;
+            $this->largeur=3;
         }
         $this->hauteur*=Edge::$grossissement;
         $this->largeur*=Edge::$grossissement;
@@ -62,8 +61,10 @@ class JM extends Edge {
             list($rouge_texte,$vert_texte,$bleu_texte)=$this->getColorsFromDB($blanc, 'Texte');
 
             imagefill($this->image, 0, 0, $fond);
-            list($texte,$width,$height)=imagecreatefrompng_getimagesize($this->getChemin().'/JM.'.$this->numero.'.Texte1.png');
-
+            if (file_exists($this->getChemin().'/JM.'.$this->numero.'.Texte1.png'))
+                list($texte,$width,$height)=imagecreatefrompng_getimagesize($this->getChemin().'/JM.'.$this->numero.'.Texte1.png');
+            else
+                return $this->image;
             imagealphablending($texte, false);
 		    $transparent = imagecolorallocatealpha($texte, 0, 0, 0, 127);
 		    imagefill($texte, 0, 0, $transparent);
