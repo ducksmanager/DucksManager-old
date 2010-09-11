daa763916e2c060449c17f61f1f5ac53b5cedd1a
brunoperel <brunoperel@f9fbd3f4-65e0-4270-b5f8-9c5e5b93a1e4>
2010-09-11 19:33:01 +0000

    Ajout LTSMG (Life and Times of Scrooge McDuck) 1 et 2

---
+++ edges/us/LTSMG.edge.class.php
@@ -0,0 +1,42 @@
+<?php
+class LTSMG extends Edge {
+    var $pays='us';
+    var $magazine='LTSMG';
+    var $intervalles_validite=array('1','2');
+    var $en_cours=array();
+    static $largeur_defaut=20;
+    static $hauteur_defaut=258;
+
+
+    function LTSMG($numero) {
+        $this->numero=$numero;
+        switch($this->numero) {
+            case '1':
+                $this->largeur=16*Edge::$grossissement;
+            break;
+            case '2':
+                $this->largeur=12.5*Edge::$grossissement;
+        }
+        $this->hauteur=258*Edge::$grossissement;
+        
+        $this->image=imagecreatetruecolor(intval($this->largeur),intval($this->hauteur));
+        if ($this->image===false)
+            xdebug_break ();
+    }
+
+    function dessiner() {
+        switch($this->numero) {
+            case '1':
+                $fond=imagecolorallocate($this->image, 247,186,79);
+            break;
+            case '2':
+                $fond=imagecolorallocate($this->image, 223,91,68);
+
+        }
+        imagefill($this->image,0,0,$fond);
+        $this->placer_image('LTSMG'.$this->numero.'.tranche.png');
+        
+        return $this->image;
+    }
+}
+?>
