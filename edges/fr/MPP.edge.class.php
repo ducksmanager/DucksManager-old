c146332ef9644823b3bd67b75ca1fbe6f3da284a
brunoperel <brunoperel@f9fbd3f4-65e0-4270-b5f8-9c5e5b93a1e4>
2010-10-03 11:14:20 +0000

    Ajout MPP 856,886,990,1016,1055,1111,1121 et MPNT 856,886

--- edges/fr/MPP.edge.class.php
+++ edges/fr/MPP.edge.class.php
@@ -2,20 +2,30 @@
 class MPP extends Edge {
     var $pays='fr';
     var $magazine='MPP';
-    var $intervalles_validite=array(723,735,756,786,807,824,838/*,856,886*/);
+    var $intervalles_validite=array(723,735,756,786,807,824,838,856,886,990,1016,1055,1111,1121,/*1134,1154,1166,1174,1182*/);
 
     static $largeur_defaut=15;
     static $hauteur_defaut=186;
 
-    function MPP($numero) {
+    function MPP ($numero) {
         $this->numero=$numero;
 
-        $this->largeur=15*Edge::$grossissement;
         $this->hauteur=186*Edge::$grossissement;
+        switch($this->numero) {
+            case 824: case 856: case 886 :
+                $this->largeur=13*Edge::$grossissement;
+            break;
+            case 838:
+                $this->largeur=14*Edge::$grossissement;
+            default:
+                $this->largeur=15*Edge::$grossissement;
+            break;
+        }
         $this->image=imagecreatetruecolor(intval($this->largeur),intval($this->hauteur));
         if ($this->image===false)
             xdebug_break ();
     }
+
     function dessiner() {
         include_once($this->getChemin().'/../../MyFonts.Post.class.php');
         $image2=imagecreatetruecolor($this->hauteur, $this->largeur);
@@ -35,9 +45,26 @@ class MPP extends Edge {
         $chemin_image=$post->chemin_image;
         list($texte,$width,$height)=imagecreatefromgif_getimagesize($chemin_image);
         $nouvelle_largeur=$this->largeur*($width/$height);
-        //$this->placer_image($chemin_image,'haut',array(1.5*$this->largeur,0));
-        $dst_x=$this->numero <= 756 ? $this->largeur*1.7 : $this->largeur*2;
-        $dst_y=$this->numero == 807 || $this->numero == 824 ? $this->largeur*0.2 : $this->largeur * 0.35;
+        switch($this->numero) {
+            case 723: case 735 : case 756 :
+                $dst_x=$this->largeur*1.7;
+            break;
+            default:
+                $dst_x=$this->largeur*2;
+            break;
+        }
+
+        switch($this->numero) {
+            case 807: case 824 :
+                $dst_y=$this->largeur*0.2;
+            break;
+            case 838 : case 856:
+                $dst_y=$this->largeur*0.42;
+            break;
+            default:
+                $dst_y=$this->largeur * 0.35;
+            break;
+        }
         imagecopyresampled ($image2, $texte, $dst_x, $dst_y, 0, 0, $nouvelle_largeur*0.37, $this->largeur*0.34, $width/2, $height/2);
 
         $post=new MyFonts('fontbureau/benton-sans/bold',
@@ -51,7 +78,17 @@ class MPP extends Edge {
         $nouvelle_largeur=$this->largeur*($width/$height);
         imagecopyresampled ($image2, $texte_r, $dst_x+$this->largeur*1.38, $dst_y, 0, 0, $nouvelle_largeur*0.34, $this->largeur*0.3, $width*0.5, $height*0.5);
 
-        $dst_x=$this->numero <= 756 ? $this->largeur*8.2 : $this->largeur*7.8;
+        switch($this->numero) {
+            case 723: case 735 : case 756 :
+                $dst_x=$this->largeur*8.2;
+            break;
+            case 856: case 1016 : case 1055: case 1111: case 1121:
+                $dst_x=$this->largeur*7.2;
+            break;
+            default:
+                $dst_x=$this->largeur*7.8;
+             break;
+        }
         switch($this->numero) {
             case 735:
                 $titre2='DONALD-PA   ADE';
@@ -84,9 +121,20 @@ class MPP extends Edge {
             $this->image=imagerotate($image2, -90, $blanc);
         else
             $this->image=imagerotate($image2, 90, $blanc);
-        $this->placer_image('MPNT.'.$this->numero.'.Etoile.png','bas',array(0,$this->hauteur*0.57));
+        switch($this->numero) {
+            case 856 :
+                $position_etoile=$this->hauteur*0.43;
+            break;
+            case 1055: case 1111: case 1121:
+                $position_etoile=$this->hauteur*0.55;
+            break;
+            default:
+                $position_etoile=$this->hauteur*0.57;
+
+        }
+        $this->placer_image('MPP.'.$this->numero.'.Etoile.png','bas',array(0,$position_etoile));
 
         return $this->image;
     }
-
 }
+?>
