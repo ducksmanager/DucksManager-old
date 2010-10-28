7770c4d94021ccedd53ec4b185107be11f5f5c9c
brunoperel <brunoperel@f9fbd3f4-65e0-4270-b5f8-9c5e5b93a1e4>
2010-10-28 16:35:42 +0000

    DMTable => CollecTable

---
+++ Listes/Liste.CollecTable.class.php
@@ -0,0 +1,235 @@
+<?php
+@session_start();
+if (isset($_GET['lang'])) {
+	$_SESSION['lang']=$_GET['lang'];
+}
+include_once ('locales/lang.php');
+include_once ('Database.class.php');
+include_once ('Magazine.class.php');
+require_once('Format_liste.php');
+class CollecTable extends Format_liste {
+    static $regex_numero_double='#([0-9]{2})([0-9]{2})\-([0-9]{2})#is';
+    static $max_centaines=0;
+    static $max_diz_et_unites=1;
+    function CollecTable() {
+        $this->les_plus=array(COLLECTABLE_PLUS_1,COLLECTABLE_PLUS_2,COLLECTABLE_PLUS_3);
+        $this->les_moins=array(COLLECTABLE_MOINS_1,COLLECTABLE_MOINS_2);
+        $this->description=COLLECTABLE_DESCRIPTION;
+    }
+
+    function afficher($liste) {
+            ?><table rules="all" style="border:1px solid gray;color: black; font:11px/15px verdana,arial,sans-serif;">
+                <tr>
+                    <td></td><?php
+            for ($i=1;$i<=50;$i++) {?>
+                    <td><?=$i?></td>
+            <?php }?>
+                    <td rowspan="2">
+                        <?=TOTAL?>
+                    </td>
+                </tr>
+                <tr>
+                    <td></td><?php
+            for ($i=51;$i<=100;$i++) {?>
+                    <td><?=$i?></td>
+            <?php }?>
+                </tr><?php
+            global $centaines_utilisees;
+            $centaines_utilisees=array();
+            ksort($liste);
+            foreach($liste as $pays=>$numeros_pays) {
+                ksort($numeros_pays);
+                    foreach($numeros_pays as $magazine=>$numeros) {
+                       $requete_get_ne_parait_plus='SELECT NeParaitPlus FROM magazines WHERE PaysAbrege LIKE \''.$pays.'\' AND NomAbrege LIKE \''.$magazine.'\'';
+                       $resultat_get_ne_parait_plus=DM_Core::$d->requete_select($requete_get_ne_parait_plus);
+                       $ne_parait_plus=$resultat_get_ne_parait_plus[0]['NeParaitPlus']==1;
+                       $montrer_numeros_inexistants=false;
+                       if ($ne_parait_plus) {
+                            list($numeros_inducks,$sous_titres)=Inducks::get_numeros($pays,$magazine);
+                            $numeros_centaines=array_map('get_nb_centaines',$numeros_inducks);
+                            $numero_centaines_min=min($numeros_centaines);
+                            $numero_centaines_max=max($numeros_centaines);
+                            $montrer_numeros_inexistants=$numero_centaines_min==$numero_centaines_max;
+                            foreach($numeros_inducks as $numero_inducks)
+                                if (!est_valide($numero_inducks))
+                                    $montrer_numeros_inexistants=false;
+                       }
+                       $total_magazine = 0;
+                       global $liste_numeros;
+                       global $liste_numeros_doubles;
+                       $liste_numeros = array();
+                       $liste_numeros_doubles = array();
+                       $liste_non_numeriques = array();
+                       foreach($numeros as $numero_et_etat) {
+                            $total_magazine++;
+                            $numero = $numero_et_etat[0];
+                            if (est_double($numero)) {
+                                preg_match(CollecTable::$regex_numero_double, $numero, $numero);
+                                $premier_numero = $numero[1] . $numero[2];
+                                $deuxieme_numero = $numero[1] . $numero[3];
+                                ajouter_a_liste($premier_numero, true);
+                                ajouter_a_liste($deuxieme_numero, true);
+                            }
+                            else {
+                                if (!est_valide($numero)) {
+                                    array_push($liste_non_numeriques, $numero);
+                                    continue;
+                                }
+                                ajouter_a_liste($numero, false);
+                            }
+                        }
+                        ?>
+                        <tr>
+                            <td rowspan="2" valign="middle" align="center">
+                                <img alt="<?=$pays?>" src="images/flags/<?=$pays?>.png" />
+                                <br />
+                                <?=$magazine?>
+                                <br />
+                            </td>
+                        <?php
+
+                        for($i=1;$i<=100;$i++) {
+                            ?><td style="<?php 
+                            if ($montrer_numeros_inexistants && !in_array($numero_centaines_min*100+$i,$numeros_inducks)) {
+                                ?>background-color:gray;<?php
+                            }
+                            ?>"><?php
+                            for ($j=0;$j<=CollecTable::$max_centaines;$j++) {
+                                if (array_key_exists($j*100+$i,$liste_numeros)) {
+                                    for ($k=0;$k<$liste_numeros[$j*100+$i];$k++) {
+                                        echo number_to_letter($j);
+                                        if (array_key_exists($j*100+$i, $liste_numeros_doubles)) {
+                                            if (array_key_exists($j*100+$i+1, $liste_numeros_doubles))
+                                                echo '&gt;';
+                                            else
+                                                echo '&lt;';
+                                        }
+                                    }
+                                }
+                            }
+                            ?></td><?php
+                            if ($i==50) {
+                                ?><td rowspan="2"><?=$total_magazine.(($ne_parait_plus)?'!':'')?></td></tr><tr><?php
+                            }
+                        }
+
+                        /*else {
+                                echo '<td>'.$total_magazine.'</td></tr><tr>';
+                        }*/
+                        ?></tr><?php
+                        if (count($liste_non_numeriques)>0) {
+                            ?><tr>
+                                <td></td>
+                                <td colspan="51">
+                                    <?=NON_NUMERIQUES?> : <?php
+                            $debut=true;
+                            foreach($liste_non_numeriques as $numero) {
+                                if (!$debut)
+                                        echo ' ; ';
+                                echo $numero;
+                                $debut=false;
+                            }
+                            ?>
+                                </td>
+                            </tr><?php
+                        }
+
+                    }
+            }
+
+            ?></table>
+            <table style="width:100%">
+                <tr>
+                    <td><?php
+            if (count($centaines_utilisees)>0) {
+                        ?><table style="color:black;font:11px/15px verdana,arial,sans-serif">
+                            <tr>
+                                <td colspan="2" align="center">
+                                    <u><?=LEGENDE_NUMEROS?></u>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="vertical-align:top"><?php
+                for ($i=0;$i<=CollecTable::$max_centaines;$i++) {
+                    if ($i==intval(CollecTable::$max_centaines/2)+1) {
+                                ?></td><td style="vertical-align:top"><?php
+                    }
+                    ?><?=number_to_letter($i)?>:<?=($i*100+1)?>-&gt;<?=(($i+1)*100)?><br /><?php
+                }
+                                ?></td></tr></table><?php
+            }
+            ?>
+                    </td><?php
+            $nb_magazines=0;
+            foreach($liste as $pays=>$numeros_pays)
+                $nb_magazines+=count($numeros_pays);
+
+            if ($nb_magazines > 1) {
+                ?>
+                    <td align="right" style="vertical-align: top">
+                        <table style="color:black;font:11px/15px verdana,arial,sans-serif"><?php
+                foreach($liste as $pays=>$numeros_pays) {
+                    ksort($numeros_pays);
+                    foreach($numeros_pays as $magazine=>$numeros) {
+                        list($nom_pays_complet,$nom_magazine_complet)=$d->get_nom_complet_magazine($pays, $magazine,true);
+                        ?><tr>
+                            <td><?=$magazine?></td>
+                            <td><?=$nom_magazine_complet?></td>
+                        </tr><?php
+                    }
+                }?>
+                        </table>
+                    </td>
+      <?php }?>
+                </tr>
+            </table>
+            <?php
+    }
+}
+function number_to_letter($number) {
+    global $centaines_utilisees;
+    $centaines_utilisees[$number]=true;
+    if ($number<26)
+        return chr(97+$number);
+    else
+        return chr(65-26+$number);
+}
+
+function est_double($numero) {
+    return preg_match(CollecTable::$regex_numero_double, $numero) > 0;
+}
+
+function est_valide($numero) {
+    return is_numeric($numero);
+}
+
+function ajouter_a_liste($numero,$est_double=false) {
+    global $liste_numeros;global $liste_numeros_doubles;
+    if (false!=(array_key_exists($numero,$liste_numeros)))
+        $liste_numeros[$numero]++;
+    else
+        $liste_numeros[$numero]=1;
+
+    $centaine=get_nb_centaines($numero);
+    $diz_et_unites=$numero-100*$centaine;
+    if ($diz_et_unites == 0) {
+        $diz_et_unites = 100;
+        $centaine--;
+    }
+    if ($centaine > CollecTable::$max_centaines)
+        CollecTable::$max_centaines = $centaine;
+    if ($diz_et_unites > CollecTable::$max_diz_et_unites)
+        CollecTable::$max_diz_et_unites = $diz_et_unites;
+    
+    if ($est_double) {
+        if (false!=(array_key_exists($numero,$liste_numeros_doubles)))
+            $liste_numeros_doubles[$numero]++;
+        else
+            $liste_numeros_doubles[$numero]=1;
+    }
+}
+
+function get_nb_centaines($numero) {
+    return intval($numero/100);
+}
+?>
--- print.php
+++ print.php
@@ -1,70 +1,115 @@
-<?php
-header('Content-Type: text/html; charset=utf-8');
+<?php header('Content-Type: text/html; charset=utf-8');
 session_start();
 if (isset($_GET['lang'])) {
-	$_SESSION['lang']=$_GET['lang'];
+    $_SESSION['lang'] = $_GET['lang'];
 }
 include_once ('locales/lang.php');
+if (!isset($_SESSION['user'])) {
+    echo IDENTIFICATION_OBLIGATOIRE;
+    exit(0);
+}
 require_once('Liste.class.php');
 require_once('JS.class.php');
+require_once('Database.class.php');
+
+global $d;
+$d = new Database();
+$d->requete('SET NAMES UTF8');
+$d->requete_select('SELECT DISTINCT Pays,Magazine,Numero,Etat,ID_Acquisition,AV,ID_Utilisateur FROM numeros WHERE (ID_Utilisateur=1) ORDER BY Pays, Magazine, Numero');
+if (!$d) {
+    echo PROBLEME_BD;
+    exit(-1);
+}
+$id_user = $d->user_to_id($_SESSION['user']);
+global $l;
+$l = $d->toList($id_user);
+
+if (isset($_POST['magazine'])) {
+    afficher_boite($_POST['pays'], $_POST['magazine']);
+    exit(0);
+}
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
-<head>
-  <meta content="text/html; charset=ISO-8859-1"
- http-equiv="content-type">
-  <title>DucksManager : <?=IMPRESSION_COLLECTION?></title>
-  <link rel="stylesheet" type="text/css" href="style.css" />
-  <!--[if IE]>
-	<style type="text/css" media="all">@import "fix-ie.css";</style>
-  <![endif]-->
-  <link rel="stylesheet" type="text/css" href="scriptaculous.css" />
-  <link rel="icon" type="image/png" href="favicon.png" />
-  <?php
-  new JS('prototype.js');
-  new JS('js/scriptaculous/src/scriptaculous.js');
-  new JS('js/my_scriptaculous.js');
-  new JS('js/ajax.js');?>
-</head>
-<body onload="implement_drags();observe_options_clicks();get_types_listes();">
-<?php
-$d=new Database();
-if (!$d) {
-	echo PROBLEME_BD;
-	exit(-1);
-}
-$id_user=$d->user_to_id($_SESSION['user']);
-$l=$d->toList($id_user);
-foreach($l->collection as $pays=>$magazines) {
-	$i=new Inducks();
-	$noms_complets_magazines=$i->get_noms_complets_magazines($pays);
-	foreach($magazines as $nom_magazine=>$magazine) {
-		$type_liste_affichage='DMtable';
-		/*if ($nom_magazine=='D') {
-			$sous_liste=new Liste();
-			$sous_liste=$l->sous_liste($pays,$nom_magazine);
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <title>DucksManager : <?= IMPRESSION_COLLECTION ?></title>
+        <link rel="stylesheet" type="text/css" href="style.css" />
+        <!--[if IE]>
+              <style type="text/css" media="all">@import "fix-ie.css";</style>
+        <![endif]-->
+        <link rel="stylesheet" type="text/css" href="scriptaculous.css" />
+        <link rel="icon" type="image/png" href="favicon.png" />
+        <link rel="stylesheet" href="protomenu.css" type="text/css" media="screen">
+        <?php
+        new JS('prototype.js');
+        new JS('js/l10n.js');
+        new JS('js/menu_contextuel.js');
+        new JS('js/scriptaculous/src/scriptaculous.js');
+        new JS('js/my_scriptaculous.js');
+        new JS('js/ajax.js'); ?>
 
-			$sous_liste2=new Liste();
-			$sous_liste2=$l->sous_liste('fr','JM');
-			$sous_liste->fusionnerAvec($sous_liste2);
-		}
-		else {*/
-			$sous_liste=new Liste();
-			$sous_liste=$l->sous_liste($pays,$nom_magazine);
-		//}
-		//echo '<div id="'.$pays.'_'.$nom_magazine.'" ><span>';
-		echo '<span id="type_liste'.$pays.'_'.$nom_magazine.'" style="display:none">'.$type_liste_affichage.'</span>';
-		echo '<table class="draggable_box"><tr><td><table width="100%"><tr><td valign="top">';
-		echo $noms_complets_magazines[$nom_magazine];
-		echo '</td><td width="140">';
-		echo '<div id="options'.$pays.'_'.$nom_magazine.'" class="options_text">&nbsp;+&nbsp;</div><p id="box_options'.$pays.'_'.$nom_magazine.'" class="box_options"></p>';
-		echo '</td></tr></table>';
-		echo '</td></tr><tr id="'.$pays.'_'.$nom_magazine.'"><td>';
-		echo $sous_liste->afficher($type_liste_affichage);
-		echo '</td><td valign="top">';
-		echo '</td></tr></table><br /><br />';
-	}
-}
+    </head>
+    <body onload="implement_dragsanddrops()" id="body">
+        <?php
+        foreach ($l->collection as $pays => $magazines) {
+            foreach ($magazines as $nom_magazine => $magazine) {
+                afficher_boite($pays,$nom_magazine);
+            }
+        }
 ?>
-</body>
+        <a id="lien_cacher_aide" class="toggle_info" href="javascript:void(0)" onclick="toggle_aide()">&lt;&lt; <span name="cacher_aide"></span></a>
+        <a id="lien_afficher_aide" class="toggle_info cache" href="javascript:void(0)" onclick="toggle_aide()"><span name="afficher_aide"></span> &gt;&gt;</a>
+        <div id="info">
+            <div style="text-align:center"><?=IMPRIMER_AIDE_TITRE?></div>
+            <hr />
+            <div id="contenu_info">
+                <?=IMPRIMER_AIDE_1?>
+                <br /><br />
+                <?=IMPRIMER_AIDE_2?>
+                <br /><br />
+                <?=IMPRIMER_AIDE_3?>
+                <br /><br />
+                <?=IMPRIMER_AIDE_4?>
+                <br /><br /><br />
+                <?=IMPRIMER_AIDE_5?>
+                <br /><br />
+                <button onclick="imprimer()"><?=IMPRIMER_COLLECTION?></button>
+            </div>
+        </div>
+    </body>
 </html>
+
+<?php
+function afficher_boite($pays,$nom_magazine) {
+    global $d;
+    global $l;
+    list($nom_complet_pays, $nom_complet_magazine) = $d->get_nom_complet_magazine($pays, $nom_magazine);
+    $type_liste_affichage = 'CollecTable';
+    $sous_liste = new Liste();
+    $sous_liste = $l->sous_liste($pays, $nom_magazine);
+    ?><div class="draggable_box widget" id="box_<?= $pays ?>_<?= $nom_magazine ?>">
+        <table>
+            <tr>
+                <td>
+                    <table width="100%">
+                        <tr>
+                            <td valign="top" class="titre_magazine" name="<?= $nom_complet_magazine ?>">
+                                <?= $nom_complet_magazine ?>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+            <tr id="<?= $pays ?>_<?= $nom_magazine ?>">
+                <td id="<?= $pays ?>_<?= $nom_magazine ?>_contenu" name="<?= $type_liste_affichage ?>" class="contenu_liste">
+<?= $sous_liste->afficher($type_liste_affichage) ?>
+                </td>
+                <td valign="top">
+                </td>
+            </tr>
+        </table>
+    </div>
+    <div class="espacement" id="espacement_<?=$pays?>_<?=$nom_magazine?>"><br /><br /></div>
+<?php
+}
