345cc4b0e6533f261b0964209d537353d051408e
brunoperel <brunoperel@f9fbd3f4-65e0-4270-b5f8-9c5e5b93a1e4>
2010-11-08 14:55:37 +0000

    Ajout MP 272

--- edges/fr/MP.edge.class.php
+++ edges/fr/MP.edge.class.php
@@ -13,8 +13,7 @@ class MP extends Edge {
                                     array('debut'=>229,'fin'=>235),
                                     array('debut'=>236,'fin'=>253),
                                     array('debut'=>255,'fin'=>256),258,259,260,262,
-                                    array('debut'=>265,'fin'=>271),
-                                    array('debut'=>273,'fin'=>288),array('debut'=>290,'fin'=>318));
+                                    array('debut'=>265,'fin'=>288),array('debut'=>290,'fin'=>318));
     
     static $largeur_defaut=20;
     static $hauteur_defaut=219.7;
