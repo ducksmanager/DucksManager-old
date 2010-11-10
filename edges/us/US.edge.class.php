8e6f57f6c434da3ec06beb4bdb701e6cdd1b5848
brunoperel <brunoperel@f9fbd3f4-65e0-4270-b5f8-9c5e5b93a1e4>
2010-11-10 13:53:10 +0000

    Ajout Uncle Scrooge 382

--- edges/us/US.edge.class.php
+++ edges/us/US.edge.class.php
@@ -2,7 +2,7 @@
 class US extends Edge {
     var $pays='us';
     var $magazine='US';
-    var $intervalles_validite=array('339','342','359','365');
+    var $intervalles_validite=array('339','342','359','365','382');
     var $en_cours=array();
     static $largeur_defaut=4;
     static $hauteur_defaut=258;
@@ -19,17 +19,18 @@ class US extends Edge {
     }
 
     function dessiner() {
-        $noir=imagecolorallocate($this->image, 0, 0, 0);
+        list($rouge_texte,$bleu_texte,$vert_texte)=$this->getColorsFromDB(array(0,0,0), 'Texte');
+        $couleur_texte=imagecolorallocate($this->image, $rouge_texte,$bleu_texte,$vert_texte);
         list($rouge,$vert,$bleu)=$this->getColorsFromDB();
         $fond=imagecolorallocate($this->image, $rouge,$vert,$bleu);
         imagefill($this->image,0,0,$fond);
         $texte=new Texte('WALT DISNEY\'S UNCLE SCROOGE '.$this->numero,$this->largeur*8/10,$this->hauteur-$this->largeur*2.5,
-                            2*Edge::$grossissement,90,$noir,'Gill Sans Bold.ttf');
+                            2*Edge::$grossissement,90,$couleur_texte,'Gill Sans Bold.ttf');
         $texte->dessiner($this->image);
 
         
         $texte=new Texte('GEMSTONE',$this->largeur*8/10,$this->largeur*6,
-                            2*Edge::$grossissement,90,$noir,'Gill Sans Bold.ttf');
+                            2*Edge::$grossissement,90,$couleur_texte,'Gill Sans Bold.ttf');
         $texte->dessiner($this->image);
         
         return $this->image;
