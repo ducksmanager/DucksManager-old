17df08e8073611f279b97e6a721193bfcc772e33
brunoperel <brunoperel@f9fbd3f4-65e0-4270-b5f8-9c5e5b93a1e4>
2010-11-26 11:16:10 +0000

    Ajout PMHS D1 (Picsoudoku)

--- edges/fr/PMHS.edge.class.php
+++ edges/fr/PMHS.edge.class.php
@@ -2,80 +2,44 @@
 class fr_PMHS extends Edge {
     var $pays='fr';
     var $magazine='PMHS';
-    var $intervalles_validite=array('B1','B2','B3','C1','C2','C3','C4','C5','C6','C7','C8','C9','CA');
-
-    static $largeur_defaut=15;
-    static $hauteur_defaut=278;
-
-    var $serie;
-    var $numero_serie;
-
+    var $intervalles_validite=array('D1');
+    static $largeur_defaut=6;
+    static $hauteur_defaut=274;
+    var $lettre;
+    
     function fr_PMHS ($numero) {
         $this->numero=$numero;
-        $this->serie=$numero[0];
-        $this->numero_serie=substr($this->numero, strrpos($this->numero, ' ')+1, strlen($this->numero));
-        
-
-        $this->hauteur=278*Edge::$grossissement;
-        $this->largeur=15*Edge::$grossissement;
-
+        $numero_str=str_split($this->numero);
+        $this->lettre=$numero_str[0];
+        switch($this->lettre) {
+            case 'D':
+                $this->largeur=6*Edge::$grossissement;
+                $this->hauteur=274*Edge::$grossissement;
+            break;
+        }
         $this->image=imagecreatetruecolor(intval($this->largeur),intval($this->hauteur));
         if ($this->image===false)
             xdebug_break ();
     }
-
     function dessiner() {
-
-        if ($this->serie == 'B' && $this->numero_serie == 1) {
-            $this->placer_image('PMHS.B1.tranche.png');
-            return $this->image;
-        }
-        include_once($this->getChemin().'/../../MyFonts.Post.class.php');
-        switch($this->serie) {
-            case 'B': case 'C':
-                if ($this->serie == 'B') {
-                    $texte='La Jeunesse de Picsou';
-                    $auteur='D O N  R O S A';
-                    $police_auteur='Compacta Bold Italic.ttf';
-                    list($rouge_auteur,$vert_auteur,$bleu_auteur)=$this->getColorsFromDB(array(255,255,255),'Texte auteur');
-                    $couleur_texte_auteur=imagecolorallocate($this->image, $rouge_auteur,$vert_auteur,$bleu_auteur);
-                }
-                else {
-                    $texte='Les T';
-                    if ($this->serie == 'C' && $this->numero_serie==1)
-                        $auteur='D O N  R O S A';
-                    else
-                        $auteur='D   I   S   N   E   Y';
-                    $police_auteur='Compacta Bold.ttf';
-                    $couleur_texte_auteur=imagecolorallocate($this->image, 230,216,117);
-                }
+        switch($this->lettre) {
+            case 'D':
                 list($rouge,$vert,$bleu)=$this->getColorsFromDB(array(0,0,0));
                 $fond=imagecolorallocate($this->image, $rouge, $vert, $bleu);
-                list($rouge_texte,$vert_texte,$bleu_texte)=$this->getColorsFromDB(array(255,255,255),'Texte');
-                $couleur_texte=imagecolorallocate($this->image, $rouge_texte, $vert_texte, $bleu_texte);
+                imagefill($this->image, 0, 0, $fond);
+                $this->placer_image('PMHS.'.$this->numero.'.Titre.png','haut',array(0,$this->hauteur*0.29));
+                list($rouge,$vert,$bleu)=$this->getColorsFromDB(array(255,255,255),'Couleur 2');
+                $couleur2=imagecolorallocate($this->image, $rouge, $vert, $bleu);
+                imageline($this->image, 0, $this->largeur*4.5, $this->largeur, $this->largeur*4.2,$couleur2);
+                imagefill($this->image,$this->largeur/2,$this->largeur,$couleur2);
+                imageline($this->image, 0, $this->hauteur-$this->largeur*0.3, $this->largeur, $this->hauteur-$this->largeur*0.4,$couleur2);
+                imagefill($this->image,$this->largeur/2,$this->hauteur-$this->largeur*0.1,$couleur2);
                 
-
-                imagefill($this->image,0,0,$fond);
-                $this->placer_image('Logo PM PMHS.png','haut',array(0,$this->largeur*0.3));
-
-                $texte=new Texte($texte,$this->largeur*7/10,$this->hauteur*0.7,
-                                6.5*Edge::$grossissement,90,$couleur_texte,'Incised.ttf');
-                $texte->dessiner($this->image);
-
-                if ($this->serie == 'C') {
-                    $texte=new Texte('résors de Picsou',$this->largeur*7/10,$this->hauteur*0.575,
-                                6.5*Edge::$grossissement,90,$couleur_texte,'Incised.ttf');
-                $texte->dessiner($this->image);
-                }
-
-                $texte=new Texte($auteur,$this->largeur*7/10,$this->hauteur-$this->largeur*1.8,
-                            5.5*Edge::$grossissement,90,$couleur_texte_auteur,$police_auteur);
-                $texte->dessiner($this->image);
-                
-                $this->placer_image('Tete PM.png','bas',array($this->largeur*0.15,0),0.7,0.7);
+                $this->placer_image('Tete PM.png', 'bas', array(0,$this->largeur/2));
             break;
         }
+            
         return $this->image;
     }
+
 }
-?>
