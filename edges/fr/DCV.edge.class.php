857435cbcc5e6c600e8d8ff1648a8b9304c934b6
brunoperel <brunoperel@f9fbd3f4-65e0-4270-b5f8-9c5e5b93a1e4>
2011-01-22 08:55:26 +0000

    Bugfix : inversion des dimensions des DCV inférieurs et supérieurs à 15

--- edges/fr/DCV.edge.class.php
+++ edges/fr/DCV.edge.class.php
@@ -9,12 +9,12 @@ class fr_DCV extends Edge {
     function fr_DCV ($numero) {
         $this->numero=$numero;
         if ($this->numero <=15) {
-            $this->largeur=5*Edge::$grossissement;
-            $this->hauteur=255*Edge::$grossissement;
-        }
-        else {
             $this->largeur=9*Edge::$grossissement;
             $this->hauteur=298*Edge::$grossissement;
+        }
+        else {
+            $this->largeur=5*Edge::$grossissement;
+            $this->hauteur=255*Edge::$grossissement;
         }
         
         $this->image=imagecreatetruecolor(intval($this->largeur),intval($this->hauteur));
