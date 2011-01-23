9ce3b924a2d3f1a8ed6d84b4ca578b26defbaa30
brunoperel <brunoperel@f9fbd3f4-65e0-4270-b5f8-9c5e5b93a1e4>
2011-01-23 18:02:41 +0000

    Ajout SMG 1408,1430,1623,1667,1727,1771

---
+++ edges/fr/SMG.edge.class.php
@@ -0,0 +1,24 @@
+<?php
+class fr_SMG extends Edge {
+    var $pays='fr';
+    var $magazine='SMG';
+    var $intervalles_validite=array(1408,1430,1623,1667,1727,1771);
+
+    static $largeur_defaut=10;
+    static $hauteur_defaut=292;
+
+    function fr_SMG ($numero) {
+        $this->numero=$numero;
+        $this->largeur=10*Edge::$grossissement;
+        $this->hauteur=292*Edge::$grossissement;
+        $this->image=imagecreatetruecolor(intval($this->largeur),intval($this->hauteur));
+        if ($this->image===false)
+            xdebug_break ();
+    }
+
+    function dessiner() {
+        
+        return $this->image;
+    }
+}
+?>
