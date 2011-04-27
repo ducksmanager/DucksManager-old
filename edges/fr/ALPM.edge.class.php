97297ca34699ac9017622bf7b05eef45747316db
brunoperel <brunoperel@f9fbd3f4-65e0-4270-b5f8-9c5e5b93a1e4>
2011-04-27 19:16:51 +0000

    Ajout ALPM B61

--- edges/fr/ALPM.edge.class.php
+++ edges/fr/ALPM.edge.class.php
@@ -2,7 +2,7 @@
 class fr_ALPM extends Edge {
     var $pays='fr';
     var $magazine='ALPM';
-    var $intervalles_validite=array('AX','B1','B7','B8','B16','B19','B25','B27','B28','B34','B40','B42','B49','B54','B57');
+    var $intervalles_validite=array('AX','B1','B7','B8','B16','B19','B25','B27','B28','B34','B40','B42','B49','B54','B57','B61');
 
     var $en_cours=array();
     static $largeur_defaut=18;
@@ -26,7 +26,7 @@ class fr_ALPM extends Edge {
                     $this->largeur=18*Edge::$grossissement;
                     $this->hauteur=260*Edge::$grossissement;
                 }
-                elseif ($this->numero_serie <= 57) {
+                else {
                     $this->largeur=18*Edge::$grossissement;
                     $this->hauteur=273*Edge::$grossissement;
                 }
@@ -38,69 +38,6 @@ class fr_ALPM extends Edge {
     }
 
     function dessiner() {
-        switch($this->serie) {
-            case 'A':
-
-            break;
-
-            case 'B':
-               if ($this->numero_serie<=27) {
-                    include_once($this->getChemin().'/../../MyFonts.Post.class.php');
-
-                    $image2=imagecreatetruecolor($this->hauteur, $this->largeur);
-                    $blanc=imagecolorallocate($image2, 255, 255, 255);
-                    list($rouge,$vert,$bleu)=$this->getColorsFromDB(array(255,255,255));
-                    switch($this->numero_serie) {
-                        case 16:
-                            $couleur1=$this->getColorsFromDB(array(0,0,0),'Degrade 2');
-                            $couleur2=$this->getColorsFromDB(array(255,255,255),'Degrade 1');
-
-                            $largeur_degrade=$this->hauteur;
-
-                            include_once($this->getChemin().'/../../util.php');
-                            $couleurs_inter=getMidColors($couleur1, $couleur2, $largeur_degrade);
-                            foreach($couleurs_inter as $i=>$couleur) {
-                                list($rouge_inter,$vert_inter,$bleu_inter)=$couleur;
-                                $couleur_allouee=imagecolorallocate($image2, $rouge_inter,$vert_inter,$bleu_inter);
-                                imageline($image2, $i, 0, $i, $this->largeur, $couleur_allouee);
-                            }
-                        break;
-                        default:
-                            $fond=imagecolorallocate($image2, $rouge, $vert, $bleu);
-                            imagefill($image2, 0, 0, $fond);
-
-                        break;    
-                    }
-                    $this->image=imagecreatetruecolor($this->hauteur, $this->hauteur);
-                    list($rouge_texte,$vert_texte,$bleu_texte)=$this->getColorsFromDB(array(255,255,255),'Texte');
-                    $post=new MyFonts('urw/nimbus-sans/l-black-condensed-italic',
-                                      rgb2hex($rouge_texte, $vert_texte, $bleu_texte),
-                                      rgb2hex($rouge, $vert, $bleu),
-                                      1200,
-                                      'ALBUM PICSOU N°'.$this->numero_serie.'      .',
-                                      84);
-                    $chemin_image=$post->chemin_image;
-                    list($texte,$width,$height)=imagecreatefromgif_getimagesize($chemin_image);
-                    $nouvelle_largeur=$this->largeur*($width/$height)*0.8;
-                    imagecopyresampled ($image2, $texte, $this->hauteur*0.2, $this->largeur*0.1, 0, 0, $nouvelle_largeur*2, $this->largeur*0.8, $width, $height*0.5);
-                    
-                    $this->image=imagerotate($image2, 90, $blanc);
-                    if ($this->numero_serie==25) {
-                        $this->placer_image ('ALPMB.'.$this->numero_serie.'.detail.png');
-                    }
-                }
-                elseif ($this->numero_serie <= 57) {
-                    list($rouge,$vert,$bleu)=$this->getColorsFromDB(array(255,255,255));
-                    $fond=imagecolorallocate($this->image, $rouge, $vert, $bleu);
-                    imagefill($this->image, 0, 0, $fond);
-                    $this->placer_image('Logo ALPBM.png');
-                    list($rouge_logo,$vert_logo,$bleu_logo)=$this->getColorsFromDB(array(255,255,255),'Logo');
-                    $fond_logo=imagecolorallocate($this->image, $rouge_logo, $vert_logo, $bleu_logo);
-                    imagefill($this->image, $this->largeur*0.66, $this->largeur, $fond_logo);
-                    $this->placer_image('ALPMB.icone.'.$this->numero_serie.'.png', 'bas');
-                }
-            break;
-        }
         
         return $this->image;
     }
