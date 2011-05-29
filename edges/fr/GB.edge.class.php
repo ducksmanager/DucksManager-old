df380b7623e74e0f8bb7b2cecf1abb802d1339c3
brunoperel <brunoperel@f9fbd3f4-65e0-4270-b5f8-9c5e5b93a1e4>
2011-05-29 19:13:33 +0000

    Ajout fr/GB 4

---
+++ edges/fr/GB.edge.class.php
@@ -0,0 +1,23 @@
+<?php
+class fr_GB extends Edge {
+	var $pays='fr';
+	var $magazine='GB';
+	var $intervalles_validite=array('4');
+	static $largeur_defaut=31;
+	static $hauteur_defaut=253;
+
+	function fr_DM ($numero) {
+		$this->numero=$numero;
+		$this->hauteur=253;
+		$this->largeur=31;
+		$this->hauteur*=Edge::$grossissement;
+		$this->largeur*=Edge::$grossissement;
+		$this->image=imagecreatetruecolor(intval($this->largeur),intval($this->hauteur));
+		if ($this->image===false)
+			xdebug_break ();
+	}
+	function dessiner() {
+		return $this->image;
+	}
+
+}
