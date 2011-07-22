87aae0592cb5d5ab7ee69a27feabc33bb3da7a06
brunoperel <brunoperel@f9fbd3f4-65e0-4270-b5f8-9c5e5b93a1e4>
2011-07-22 22:40:46 +0000

    Ajout TP 15 et DDD 3

--- edges/fr/DDD.edge.class.php
+++ edges/fr/DDD.edge.class.php
@@ -1,27 +1,27 @@
 <?php
 class fr_DDD extends Edge {
-    var $pays='fr';
-    var $magazine='DDD';
-    var $intervalles_validite=array(1,2);
-    static $largeur_defaut=30;
-    static $hauteur_defaut=247;
+	var $pays='fr';
+	var $magazine='DDD';
+	var $intervalles_validite=array(1,2,3);
+	static $largeur_defaut=30;
+	static $hauteur_defaut=247;
 
-    var $serie;
-    var $numero_serie;
+	var $serie;
+	var $numero_serie;
 
-    function fr_DDD($numero) {
-        $this->numero=$numero;
-        $this->largeur=30*Edge::$grossissement;
-        $this->hauteur=247*Edge::$grossissement;
-        
-        $this->image=imagecreatetruecolor(intval($this->largeur),intval($this->hauteur));
-        if ($this->image===false)
-            xdebug_break ();
-    }
+	function fr_DDD($numero) {
+		$this->numero=$numero;
+		$this->largeur=30*Edge::$grossissement;
+		$this->hauteur=247*Edge::$grossissement;
+		
+		$this->image=imagecreatetruecolor(intval($this->largeur),intval($this->hauteur));
+		if ($this->image===false)
+			xdebug_break ();
+	}
 
-    function dessiner() {
-        
-        return $this->image;
-    }
+	function dessiner() {
+		
+		return $this->image;
+	}
 }
 ?>
