64888ec35c70391f9053f51f65b22fc4f35485cb
Bruno Perel <admin@ducksmanager.net>
2013-02-05 21:24:59 +0100

    Mise à jour de la regex suite au stockage des noms d'utilisateurs dans les tranches prêtes au lieu des ID d'utilisateurs ; inclusion de la mention "Merci" qui était faite manuellement avant

--- edges/new_edges_query_to_forum_changelog.php
+++ edges/new_edges_query_to_forum_changelog.php
@@ -24,26 +24,37 @@ if (isset($_POST['query'])) {
 	$lignes=explode("\r\n",$_POST['query']);
 	foreach($lignes as $ligne) {
 		if (!empty($ligne)) {
-			$regex="#INSERT INTO `tranches_pretes` VALUES \('([^']+)', '([^']+)', (?:(?:NULL)|(?:'[0-9,]*')), (?:(?:NULL)|(?:'[0-9,]*')), #";
+			$regex="#INSERT INTO `tranches_pretes` VALUES \\('([^']+)', '([^']+)', ((NULL)|(?:'([^']*)')), (?:(?:NULL)|(?:'[^']*')), [^\\)]*\\);#";
 			preg_match($regex,$ligne,$matches);
 			list($pays,$magazine)=explode('/',$matches[1]);
 			$numero=$matches[2];
+			$photographes = $matches[4].$matches[5];
+			$photographes = $photographes === 'NULL' ? null : $photographes;
 			$url='http://ducksmanager.net/edges/'.$pays.'/gen/'.$magazine.'.'.$numero.'.png?'.$abc[rand(0,25)];
 			$code_images.='[img]'.$url.'[/img]';
 			if (!array_key_exists($pays.'/'.$magazine,$numeros))
 				$numeros[$pays.'/'.$magazine]=array();
-			$numeros[$pays.'/'.$magazine][]=$numero;
+			$numeros[$pays.'/'.$magazine]['numeros'][]=$numero;
+			if (!is_null($photographes)) {
+				$numeros[$pays.'/'.$magazine]['contributeurs'][]=substr($photographes,1);
+			}
 			DM_Core::$d->requete($ligne);
 		}
 	}
 	
 	list($noms_pays,$noms_magazines) = Inducks::get_noms_complets(array_keys($numeros));
-	foreach($numeros as $publicationcode=>$numeros_associes) {
+	foreach($numeros as $publicationcode=>$numeros_et_contributeurs) {
 		list($pays,$magazine)=explode('/',$publicationcode);
-		$code_ajouts[]= '[Biblioth&egrave;que][Tranches][Ajout]'
-					   .$noms_magazines[$publicationcode]
-					   .($pays=='fr' ? '':' ('.$noms_pays[$pays].')')
-					   .' n&deg; '.implode(', ',$numeros_associes);
+		$code_ajout ='[Biblioth&egrave;que][Tranches][Ajout]'
+				    .$noms_magazines[$publicationcode]
+				    .($pays=='fr' ? '':' ('.$noms_pays[$pays].')')
+				    .' n&deg; '.implode(', ',$numeros_et_contributeurs['numeros']);
+		$contributeurs=$numeros_et_contributeurs['contributeurs'];
+		if (isset($contributeurs) && !is_null($contributeurs)) {
+			$contributeurs = array_unique($contributeurs);
+			$code_ajout.= ' (Merci '.implode(', ',$contributeurs).')';
+		}
+		$code_ajouts[]=$code_ajout;
 	}
 	echo '[code]'.implode('<br />',$code_ajouts).'[/code]';
 	echo '<br /><br />';
