830f93ada422c8be27926af593eff2e6c62255e9
Bruno Perel <admin@ducksmanager.net>
2013-08-24 11:43:35 +0200

    Récupération du mot de passe de la base Inducks depuis un fichier externe dans le batch quotidien

--- remote/cron_inducks.sh
+++ remote/cron_inducks.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
-cd /var/www/inducks/isv
+sh_dir="$PWD"
+. ~/ducksmanager/inducks.properties
+cd $isv_path
 rm *.isv
 rm *.sql
 wget -rnd -l1 --no-parent http://coa.inducks.org/inducks/isv/
-cd ..
-mv isv/createtables.sql ..
-cd ..
+mv createtables.sql ..
+cd $sh_dir
 php clean_sql_inducks.php
-cd inducks
-mysql --user=root --password=xxxxxx --default_character_set utf8 coa < createtables_clean.sql
-cd ..
+cd $isv_path/..
+mysql --user=root --password=$db_password --default_character_set utf8 coa --local_infile=1 < createtables_clean.sql
+exit
+cd $sh_dir
 php insert_new_similar_edges.php
 
-mysqldump --password=xxxxxx --user=root --databases db301759616 > /var/www/dump.sql
+mysqldump --password=$db_password --user=root --databases db301759616 > /var/www/dump.sql
