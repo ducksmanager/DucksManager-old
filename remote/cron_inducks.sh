92dc987476ff857210ffbda74dda0c84a875bb0e
Bruno Perel <admin@ducksmanager.net>
2013-08-24 13:21:32 +0200

    Options de récupération des ISV Inducks

--- remote/cron_inducks.sh
+++ remote/cron_inducks.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
-sh_dir="$PWD"
 . /home/ducksmanager/inducks.properties
 cd $isv_path
 rm *.isv
 rm *.sql
-wget -rnd -l1 --no-parent http://coa.inducks.org/inducks/isv/
+wget -rNnd -l1 --no-parent http://coa.inducks.org/inducks/isv/
 mv createtables.sql ..
 cd $sh_dir
 php clean_sql_inducks.php
 cd $isv_path/..
-mysql --user=root --password=$db_password --default_character_set utf8 coa --local_infile=1 < createtables_clean.sql
+mysql -v --user=root --password=$db_password --default_character_set utf8 coa --local_infile=1 < createtables_clean.sql
 exit
 cd $sh_dir
 php insert_new_similar_edges.php
