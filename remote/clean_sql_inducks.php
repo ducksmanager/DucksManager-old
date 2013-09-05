79147d35ed866c49b19a0482c4859ede823d9329
Bruno Perel <brunoperel@gmail.com>
2013-09-05 20:50:30 +0200

    Correction répertoire de lecture/écriture du SQL pour Inducks

--- remote/clean_sql_inducks.php
+++ remote/clean_sql_inducks.php
@@ -2,12 +2,12 @@
 $no_database=true;
 include_once('../Util.class.php');
 $properties=parse_ini_file('/home/ducksmanager/ducksmanager.properties');
-$sql=Util::lire_depuis_fichier($properties['isv_path'].'/createtables.sql');
+$sql=Util::lire_depuis_fichier($properties['isv_path'].'/../createtables.sql');
 $sql=  preg_replace('#DROP TABLE IF EXISTS induckspriv[^;]+;#is', '', $sql);
 $sql=  preg_replace('#RENAME TABLE induckspriv[^;]+;#is', '', $sql);
 $sql=  preg_replace('#CREATE TABLE IF NOT EXISTS induckspriv[^;]+;#is', '', $sql);
 $sql=  preg_replace('#LOAD DATA LOCAL INFILE "\./isv/induckspriv[^;]+;#is', '', $sql);
 $sql=  preg_replace('#CREATE TABLE induckspriv_[^;]+;#is', '', $sql);
 $sql=  preg_replace('#\# SQL for re-creating and filling table induckspriv_[a-z]*#is', '', $sql);
-Util::ecrire_dans_fichier($properties['isv_path'].'/createtables_clean.sql', $sql, false);
+Util::ecrire_dans_fichier($properties['isv_path'].'/../createtables_clean.sql', $sql, false);
 ?>
