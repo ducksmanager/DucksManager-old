e7e0584c41de993c9abfd22170f2adb81b990d15
Bruno Perel <brunoperel@gmail.com>
2013-09-15 11:50:55 +0200

    Retrait de l'option -x pour les commandes utilisant le mot de passe de la base

--- remote/cron_inducks.sh
+++ remote/cron_inducks.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
+set +x
 . /home/ducksmanager/ducksmanager.properties
+set -x
+
 cd $isv_path
 rm *.isv
 rm *.sql
@@ -8,9 +11,16 @@ mv createtables.sql ..
 cd $sh_dir
 php clean_sql_inducks.php
 cd $isv_path/..
+
+set +x
+echo "mysql -v --user=root --password=xxxxxxxx --default_character_set utf8 coa --local_infile=1 < createtables_clean.sql" 1>&2
 mysql -v --user=root --password=$db_password --default_character_set utf8 coa --local_infile=1 < createtables_clean.sql
-exit
+set -x
+
 cd $sh_dir
 php insert_new_similar_edges.php
 
+set +x
+echo "mysqldump --password=xxxxxxxx --user=root --databases db301759616 > /var/www/dump.sql" 1>&2
 mysqldump --password=$db_password --user=root --databases db301759616 > /var/www/dump.sql
+set -x
