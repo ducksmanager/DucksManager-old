8458ccace661ceb44f6d2020bce28c9ff93e4726
Bruno Perel <brunoperel@gmail.com>
2013-09-16 10:22:52 +0200

    Empêcher l'affichage de l'adresse e-amil

--- remote/test_dispo_dm.sh
+++ remote/test_dispo_dm.sh
@@ -5,5 +5,8 @@ content=$(wget ducksmanager.net -q -O -)
 
 LEN=$(echo ${#content})
 if [ $LEN -lt 1000 ]; then
+	set +x
 	echo "" | mail -s "DucksManager semble indisponible (homepage : $LEN caracteres)" $admin_email
+	echo "mail -s 'DucksManager semble indisponible (homepage : $LEN caracteres)' \$admin_email"
+	set -x
 fi
