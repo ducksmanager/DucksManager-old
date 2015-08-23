4bc02b7e403165a2d140a489b83d076fa1d4cbb3
Bruno Perel <brunoperel@gmail.com>
2015-08-24 00:26:58 +0200

    Chemin des ISV passés en argument du script Bash

--- remote/cron_inducks.sh
+++ remote/cron_inducks.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
+sh_dir=`pwd`
+isv_path=$1
+
 set +x
-. /home/ducksmanager/ducksmanager.properties
+. /home/coa/coa.properties
 set -x
 
-cd $isv_path
+cd ${isv_path}
 rm *.isv
 rm *.sql
 wget -rNnd -l1 --no-parent http://coa.inducks.org/inducks/isv/
 mv createtables.sql ..
 cd $sh_dir
 php import_inducks.php 'clean'
-cd $isv_path/..
+cd ${isv_path}/..
 
 set +x
 echo "mysql -v --user=root --password=xxxxxxxx --default_character_set utf8 coa --local_infile=1 < createtables_clean.sql" 1>&2
