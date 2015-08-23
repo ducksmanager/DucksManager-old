c7832f62f5dd156a3b3263f2c1f61c6159805bd2
Bruno Perel <brunoperel@gmail.com>
2015-08-23 18:43:29 +0200

    Isolation des scripts de maintenance

---
+++ remote/check_removed_publications.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set +x
+. /home/ducksmanager/ducksmanager.properties
+set -x
+
+removed_publications_text=$(php import_inducks.php 'check_removed_publications')
+LEN=$(echo ${#removed_publications_text})
+if [ ${LEN} -gt 0 ]; then
+	set +x
+	echo ${removed_publications_text} | mail -s "DucksManager - Magazines disparus" $admin_email
+	echo "mail -s 'DucksManager - Magazines disparus' \$admin_email"
+	set -x
+fi
---
+++ remote/insert_new_similar_edges.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+php insert_new_similar_edges.php
