201976d22950ebafa700d96971e65531093e1cde
Bruno Perel <bperel@users.noreply.github.com>
2015-08-24 15:55:51 +0200

    Correction script cron

--- remote/cron_inducks.sh
+++ remote/cron_inducks.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-sh_dir=`pwd`
+sh_dir=`pwd`/remote
 isv_path=$1
+properties_file=/home/coa/coa.properties
 
 set +x
-. /home/coa/coa.properties
+. ${properties_file}
 set -x
 
 cd ${isv_path}
@@ -12,9 +13,9 @@ rm *.sql
 wget -rNnd -l1 --no-parent http://coa.inducks.org/inducks/isv/
 mv createtables.sql ..
 cd $sh_dir
-php import_inducks.php 'clean'
+php import_inducks.php 'clean' "${properties_file}" "${isv_path}"
 cd ${isv_path}/..
 
 set +x
 echo "mysql -v --user=root --password=xxxxxxxx --default_character_set utf8 coa --local_infile=1 < createtables_clean.sql" 1>&2
-mysql -v --user=root --password=$db_password --default_character_set utf8 coa --local_infile=1 < createtables_clean.sql
+mysql -v --user=root --password=$DB_PASSWORD --default_character_set utf8 coa --local_infile=1 < createtables_clean.sql
