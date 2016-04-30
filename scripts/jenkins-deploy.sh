5af4b107430d4a8b22a45edbf7661a2fae34bff4
Bruno Perel <brunoperel@gmail.com>
2016-04-30 22:18:26 +0200

    Ajout du script de déploiement

---
+++ scripts/jenkins-deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+rm -f changeset.xml
+rm -rf files changeset.txt
+wget "$JENKINS_URL/api/xml?depth=2&xpath=/hudson/job[name='$JOB_NAME']/build[id='$BUILD_ID']/changeSet" --output-document=changeset.xml
+
+xpath -q -e "//changeSet/item/path/file[not(../editType/text() = 'delete')]/text()" changeset.xml | uniq > changeset.txt
+
+mkdir files
+cd files
+
+BASE=raw.github.com/bperel/DucksManager/master/
+mkdir -p $BASE
+
+wget -xi ../changeset.txt --base=https://$BASE
+
+
+ls -la $BASE
