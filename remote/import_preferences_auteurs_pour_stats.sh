62486c6b329bc2ed46300ca12fc8f97011f61c52
Bruno Perel <bperel@users.noreply.github.com>
2016-05-16 17:24:29 +0200

    Correction chemins de script

--- remote/import_preferences_auteurs_pour_stats.sh
+++ remote/import_preferences_auteurs_pour_stats.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-sh_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+sh_dir=$1
+
 properties_file=/home/coa/coa.properties
 export_page=http://www.ducksmanager.net/remote/generer_csv_pour_export.php
 
@@ -9,13 +10,13 @@ cd ${sh_dir}
 set +x
 . ${properties_file}
 
-echo "wget ${export_page}?mdp=$\{SECURITY_PASSWORD\}&csv=numeros -O export/numeros.csv" 1>&2
-wget "${export_page}?mdp=${SECURITY_PASSWORD}&csv=numeros" -O export/numeros.csv
+echo "wget ${export_page}?mdp=$\{SECURITY_PASSWORD\}&csv=numeros -O ${sh_dir}/export/numeros.csv" 1>&2
+wget -q "${export_page}?mdp=${SECURITY_PASSWORD}&csv=numeros" -O ${sh_dir}/export/numeros.csv
 
-echo "wget ${export_page}?mdp=$\{SECURITY_PASSWORD\}&csv=auteurs_pseudos -O export/auteurs_pseudos.csv" 1>&2
-wget "${export_page}?mdp=${SECURITY_PASSWORD}&csv=auteurs_pseudos" -O export/auteurs_pseudos.csv
+echo "wget ${export_page}?mdp=$\{SECURITY_PASSWORD\}&csv=auteurs_pseudos -O ${sh_dir}/export/auteurs_pseudos.csv" 1>&2
+wget -q "${export_page}?mdp=${SECURITY_PASSWORD}&csv=auteurs_pseudos" -O ${sh_dir}/export/auteurs_pseudos.csv
 
-echo "mysql -v --user=root --password=xxxxxxxx --default_character_set utf8 db301759616 --local_infile=1 < import_preferences_auteurs_pour_stats.sql" 1>&2
-mysql -v --user=root --password=${DB_PASSWORD} --default_character_set utf8 db301759616 --local_infile=1 < import_preferences_auteurs_pour_stats.sql
+echo "mysql -v --user=root --password=xxxxxxxx --default_character_set utf8 db301759616 --local_infile=1 < ${sh_dir}/import_preferences_auteurs_pour_stats.sql" 1>&2
+mysql -v --user=root --password=${DB_PASSWORD} --default_character_set utf8 db301759616 --local_infile=1 < ${sh_dir}/import_preferences_auteurs_pour_stats.sql
 
 set -x
