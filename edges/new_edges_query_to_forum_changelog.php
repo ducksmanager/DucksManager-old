67adb3521382af62a78c9015fa1d7262da98378a
Bruno Perel <brunoperel@gmail.com>
2017-10-19 23:05:48 +0200

    Corrections ajout de tranche

--- edges/new_edges_query_to_forum_changelog.php
+++ edges/new_edges_query_to_forum_changelog.php
@@ -3,22 +3,39 @@ error_reporting(E_ALL);
 include_once('../Database.class.php');
 include_once('../authentification.php');
 
-$requete_tranches_pretes_pour_publication = 'SELECT * FROM tranches_en_cours_modeles WHERE PretePourPublication=1';
+$requete_tranches_pretes_pour_publication = '
+  SELECT ID, Pays, Magazine, Numero
+  FROM tranches_en_cours_modeles modeles
+  WHERE PretePourPublication=1';
 $tranches_pretes_pour_publication = Inducks::requete_select($requete_tranches_pretes_pour_publication, 'db_edgecreator', 'serveur_virtuel');
 
 $url_gen_edgecreator = ServeurDb::getUrlServeurVirtuel().':8002';
 
-$urls_images= [];
 $numeros= [];
 
 foreach($tranches_pretes_pour_publication as $tranche) {
-    $pays = $tranche['Pays'];
     $id = $tranche['ID'];
+    $pays = $tranche['Pays'];
     $magazine = $tranche['Magazine'];
     $publicationcode = $pays.'/'.$magazine;
     $numero = $tranche['Numero'];
-    $photographes = explode(',', $tranche['photographes']);
-    $createurs = explode(',', $tranche['createurs']);
+
+    $requete_contributeurs = "SELECT ID_Utilisateur, contribution FROM tranches_en_cours_contributeurs WHERE ID_Modele=$id";
+    $contributeurs = Inducks::requete_select($requete_contributeurs, 'db_edgecreator', 'serveur_virtuel');
+
+    $photographes = [];
+    $createurs = [];
+
+    array_filter($contributeurs, function($contributeur) use(&$photographes, &$createurs) {
+        switch($contributeur['contribution']) {
+            case 'photographe':
+                $photographes[] = $contributeur['ID_Utilisateur'];
+            break;
+            case 'createur':
+                $createurs[] = $contributeur['ID_Utilisateur'];
+            break;
+        }
+    });
 
     $valeurs= [];
     $valeurs['publicationcode']=$publicationcode;
@@ -59,49 +76,19 @@ foreach($tranches_pretes_pour_publication as $tranche) {
         Util::get_service_results(ServeurCoa::$coa_servers['dedibox2'], 'POST', "/edgecreator/model/v2/$id/readytopublish/0", 'edgecreator', []);
 
     }
-    $urls_images[]=$url;
-    if (!array_key_exists($publicationcode,$numeros))
-        $numeros[$publicationcode]= ['numeros'=> [], 'contributeurs'=> []];
-    $numeros[$publicationcode]['numeros'][]=$numero;
-    if (!is_null($photographes)) {
-        $numeros[$publicationcode]['contributeurs'][]=$photographes;
-    }
-    if (!is_null($createurs)) {
-        $numeros[$publicationcode]['contributeurs'][]=$createurs;
-    }
+    $numeros[] = [
+        'publicationcode' => $publicationcode,
+        'numero' => $numero,
+        'url' => $url,
+        'photographes' => $photographes,
+        'createurs' => $createurs
+    ];
 }
 
-$noms_pays = Inducks::get_noms_complets_pays(array_keys($numeros));
-$noms_magazines = Inducks::get_noms_complets_magazines(array_keys($numeros));
+echo '<pre>'.json_encode($numeros, JSON_PRETTY_PRINT).'</pre>';
 
-$code_ajouts = [];
-$code_images_ajouts = [];
-$contributeurs_non_remercies = ['brunoperel'];
-foreach($numeros as $publicationcode=>$numeros_et_contributeurs) {
-    list($pays,$magazine)=explode('/',$publicationcode);
-    $code_ajout ='[Biblioth&egrave;que][Tranches][Ajout]'
-        .$noms_magazines[$publicationcode]
-        .($pays=='fr' ? '':' ('.$noms_pays[$pays].')')
-        .' n&deg; '.implode(', ',$numeros_et_contributeurs['numeros']);
-
-    $contributeurs=array_diff(
-        array_merge($numeros_et_contributeurs['contributeurs'][0], $numeros_et_contributeurs['contributeurs'][1]),
-        $contributeurs_non_remercies
-    );
-    if (count($contributeurs) > 0) {
-        $contributeurs = array_unique($contributeurs);
-        $code_ajout.= ' (Merci '.implode(', ',$contributeurs).')';
-    }
-    $code_ajouts[]=$code_ajout;
-    foreach($numeros_et_contributeurs['numeros'] as $numero) {
-        $code_images_ajouts[] = '[img]http://ducksmanager.net/edges/' . $pays . '/gen/' . $magazine . '.' .$numero.'.png?C[/img]';
-    }
-}
-echo '<pre>[code]'.implode('<br />',$code_ajouts).'[/code]</pre>';
-echo '<pre style="white-space: normal">'.implode('',$code_images_ajouts).'</pre>';
-echo '<br /><br />';
-foreach ($urls_images as $url_image) {
-    ?><img src="<?=$url_image?>" /><?php
+foreach ($numeros as $numero) {
+    ?><img src="<?=$numero['url']?>" /><?php
 }
 
 ?><br /><br /><?php
