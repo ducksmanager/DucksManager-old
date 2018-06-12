fb546ed8483876fad3367ff162adb7d9fc98fb55
Bruno Perel <brunoperel@gmail.com>
2018-06-12 20:27:47 +0200

    Suppression d'un warning Ajout de log

--- remote/reset_popularite_numeros.php
+++ remote/reset_popularite_numeros.php
@@ -4,5 +4,8 @@ include_once 'auth.php';
 
 $requetes = explode(';', file_get_contents('reset_popularite_numeros.sql'));
 foreach($requetes as $requete) {
-    Database::$handle->query($requete);
+    if (!empty($requete)) {
+        echo $requete.'<br />';
+        Database::$handle->query($requete);
+    }
 }
