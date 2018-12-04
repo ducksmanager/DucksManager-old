e10d59823e6b8ab4dd6e034fa49f7116786f6805
Bruno Perel <brunoperel@gmail.com>
2018-12-04 22:18:08 +0100

    Utilisation d'un service DM pour les requêtes COA de WTD

--- remote/WhatTheDuck.php
+++ remote/WhatTheDuck.php
@@ -104,13 +104,12 @@ else if (isset($_GET['pseudo_user'], $_GET['mdp_user'])) {
 		echo json_encode($retour);
 	}
 	else {
-		Inducks::$use_local_db=false;
 		// Récupération des informations sur la collection de l'utilisateur
 		$pseudo=mysqli_real_escape_string(Database::$handle, $_GET['pseudo_user']);
 		$mdp=mysqli_real_escape_string(Database::$handle, $_GET['mdp_user']);
 
 		$requete='SELECT ID FROM users WHERE username=\''.$pseudo.'\' AND password=\''.$mdp.'\'';
-		$resultats=Inducks::requete_select($requete,'db301759616','ducksmanager.net');
+        $resultats = DmClient::get_query_results_from_dm_site($requete);
 
 		if (isset($_GET['debug'])) {
             echo $requete . '<br />';
@@ -132,7 +131,7 @@ else if (isset($_GET['pseudo_user'], $_GET['mdp_user'])) {
                 $requete = "
 					  INSERT INTO users(username,password,Email,DateInscription)
 					  VALUES('$user','$pass','$email','" . date('Y-m-d') . "')";
-                $resultats = Inducks::requete_select($requete, 'db301759616', 'ducksmanager.net');
+                $resultats = DmClient::get_query_results_from_dm_site($requete);
                 if ($resultats === []) {
                     echo 'OK';
                 }
@@ -151,8 +150,12 @@ else if (isset($_GET['pseudo_user'], $_GET['mdp_user'])) {
 
                     if (isset($_GET['id_acquisition'])) {
                         $id_acquisition = $_GET['id_acquisition'];
-                        $requete_date_achat = "SELECT 1 FROM achats WHERE ID_Acquisition=$id_acquisition AND ID_User=$id_utilisateur";
-                        if (count($resultats) !== 1) {
+                        $requete_date_achat = "
+                            SELECT 1
+                            FROM achats
+                            WHERE ID_Acquisition=$id_acquisition AND ID_User=$id_utilisateur";
+                        $resultats_date_achat = DmClient::get_query_results_from_dm_site($requete_date_achat);
+                        if (count($resultats_date_achat) !== 1) {
                             echo 'Invalid purchase ID';
                         }
                     } else {
@@ -168,7 +171,7 @@ else if (isset($_GET['pseudo_user'], $_GET['mdp_user'])) {
 							  INSERT INTO numeros(Pays,Magazine,Numero, Etat, ID_Acquisition, ID_Utilisateur)
 							  VALUES('$pays', '$magazine', '$numero', '$etat', $id_acquisition, $id_utilisateur)";
                     }
-                    $resultats = Inducks::requete_select($requete, 'db301759616', 'ducksmanager.net');
+                    $resultats = DmClient::get_query_results_from_dm_site($requete);
 
                     if (isset($_GET['debug'])) {
                         echo $requete . '<br />';
@@ -185,7 +188,7 @@ else if (isset($_GET['pseudo_user'], $_GET['mdp_user'])) {
 						  INSERT INTO achats(ID_User,Date,Description)
 						  VALUES ($id_utilisateur, '$date_achat', '$description_achat')";
 
-                    $resultats_achats = Inducks::requete_select($requete_ajout_achat, 'db301759616', 'ducksmanager.net');
+                    $resultats_achats = DmClient::get_query_results_from_dm_site($requete_ajout_achat);
 
                     if (isset($_GET['debug'])) {
                         echo $requete_ajout_achat . '<br />';
@@ -196,11 +199,10 @@ else if (isset($_GET['pseudo_user'], $_GET['mdp_user'])) {
                 } else if (isset($_GET['get_achats'])) {
                     $retour = new stdClass();
                     $requete_achats = "SELECT ID_Acquisition, Date,Description FROM achats WHERE ID_User=$id_utilisateur ORDER BY Date DESC LIMIT 15";
-                    $resultats_achats = Inducks::requete_select($requete_achats, 'db301759616', 'ducksmanager.net');
+                    $resultats_achats = DmClient::get_query_results_from_dm_site($requete_achats);
                     $retour->achats = $resultats_achats;
                     echo json_encode($retour);
                 } else {
-                    ServeurDb::connect('coa');
                     $retour = new stdClass();
                     $numeros = [];
                     $pays = [];
@@ -211,7 +213,7 @@ else if (isset($_GET['pseudo_user'], $_GET['mdp_user'])) {
 						  LEFT JOIN achats ON numeros.ID_Acquisition=achats.ID_Acquisition
 						  WHERE ID_Utilisateur=$id_utilisateur
 						  ORDER BY Pays, Magazine, Numero";
-                    $resultats_numeros = Inducks::requete_select($requete_numeros, 'db301759616', 'ducksmanager.net');
+                    $resultats_numeros = DmClient::get_query_results_from_dm_site($requete_numeros);
                     foreach ($resultats_numeros as $resultat_numero) {
                         $pays_magazine = $resultat_numero['Pays'] . '/' . $resultat_numero['Magazine'];
                         if (!array_key_exists($pays_magazine, $numeros)) {
@@ -236,16 +238,17 @@ else if (isset($_GET['pseudo_user'], $_GET['mdp_user'])) {
                     }
 
                     foreach (array_keys($magazines) as $nom_abrege) {
-                        $requete_nom_complet_magazine = 'SELECT inducks_countryname.countryname as countryname, inducks_publication.title as title '
-                            . 'FROM inducks_publication '
-                            . 'INNER JOIN inducks_countryname ON inducks_publication.countrycode = inducks_countryname.countrycode '
-                            . 'WHERE inducks_countryname.languagecode=\'' . $language . '\' '
-                            . '  AND inducks_publication.publicationcode=\'' . $nom_abrege . '\'';
-                        $resultats_nom_complet_magazine = Database::$handle->query($requete_nom_complet_magazine);
-                        while ($resultat_nom_magazine = $resultats_nom_complet_magazine->fetch_array(MYSQLI_ASSOC)) {
+                        $requete_nom_complet_magazine = "
+                            SELECT inducks_countryname.countryname as countryname, inducks_publication.title as title
+                            FROM inducks_publication
+                            INNER JOIN inducks_countryname ON inducks_publication.countrycode = inducks_countryname.countrycode
+                            WHERE inducks_countryname.languagecode='$language'
+                            AND inducks_publication.publicationcode='$nom_abrege'";
+                        $resultats_nom_complet_magazine = DmClient::get_query_results_from_dm_server($requete_nom_complet_magazine, 'db_coa');
+                        foreach($resultats_nom_complet_magazine as $resultat_nom_magazine) {
                             list($nom_pays, $nom_magazine) = explode('/', $nom_abrege);
-                            $pays[$nom_pays] = $resultat_nom_magazine['countryname'];
-                            $magazines[$nom_abrege] = $resultat_nom_magazine['title'];
+                            $pays[$nom_pays] = $resultat_nom_magazine->countryname;
+                            $magazines[$nom_abrege] = $resultat_nom_magazine->title;
                         }
                     }
 
